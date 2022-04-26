3e2f6503058ae980efb18a454628d89259920ba8
Piwo96 <philipp.rollmann@online.de>
2022-04-26 22:37:00 +0200

    styling changed

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/orangutan-gifs/precache-manifest.d14fc60da665b8a6f5671d38501e1b3b.js"
+  "/orangutan-gifs/precache-manifest.5f813a4fdb4734eaff13a75afc1d1c05.js"
 );
 
 workbox.clientsClaim();
