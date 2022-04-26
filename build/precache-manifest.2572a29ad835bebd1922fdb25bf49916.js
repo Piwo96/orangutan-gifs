b19685124197869b1aed4fda50c707afaace156b
Piwo96 <philipp.rollmann@online.de>
2022-04-26 17:58:07 +0200

    scripts for website deployment added

---
+++ build/precache-manifest.2572a29ad835bebd1922fdb25bf49916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "83d825cc90c96d511a11",
+    "url": "/orangutan-gifs/static/css/main.d315d4c0.chunk.css"
+  },
+  {
+    "revision": "83d825cc90c96d511a11",
+    "url": "/orangutan-gifs/static/js/main.83d825cc.chunk.js"
+  },
+  {
+    "revision": "ad5c24c4990c28290b12",
+    "url": "/orangutan-gifs/static/js/1.ad5c24c4.chunk.js"
+  },
+  {
+    "revision": "35d83b8baf3e5c4607fa",
+    "url": "/orangutan-gifs/static/js/runtime~main.35d83b8b.js"
+  },
+  {
+    "revision": "5c97ee2a3ed91e9de148f8d40b2d324e",
+    "url": "/orangutan-gifs/static/media/twitter-logo.5c97ee2a.svg"
+  },
+  {
+    "revision": "7d76f1463abc5dff943b7420eac906ac",
+    "url": "/orangutan-gifs/index.html"
+  }
+];
