10ad9a759f23fa5714e97c49eb995097798780e7
Piwo96 <philipp.rollmann@online.de>
2022-05-04 18:28:30 +0200

    Updates

---
+++ precache-manifest.6d3c713cb2d65b077da433370e068ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30d4112083c3c483489f",
+    "url": "/orangutan-gifs/static/css/main.0b87c921.chunk.css"
+  },
+  {
+    "revision": "30d4112083c3c483489f",
+    "url": "/orangutan-gifs/static/js/main.30d41120.chunk.js"
+  },
+  {
+    "revision": "7693d6f8124f81b2db5e",
+    "url": "/orangutan-gifs/static/js/1.7693d6f8.chunk.js"
+  },
+  {
+    "revision": "35d83b8baf3e5c4607fa",
+    "url": "/orangutan-gifs/static/js/runtime~main.35d83b8b.js"
+  },
+  {
+    "revision": "5c97ee2a3ed91e9de148f8d40b2d324e",
+    "url": "/orangutan-gifs/static/media/twitter-logo.5c97ee2a.svg"
+  },
+  {
+    "revision": "35dd2a5644f11f357e522571b630f608",
+    "url": "/orangutan-gifs/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/orangutan-gifs/precache-manifest.d14fc60da665b8a6f5671d38501e1b3b.js"
+  "/orangutan-gifs/precache-manifest.6d3c713cb2d65b077da433370e068ef3.js"
 );
 
 workbox.clientsClaim();
